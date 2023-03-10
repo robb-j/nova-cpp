e37c84a09cad9e8df9eed35d94b4706a7092fb1d
Rob Anderson <robanderson@hey.com>
2023-03-10 16:11:27 +0000

    fix ci & run formatter

--- src/extension/utils.ts
+++ src/extension/utils.ts
@@ -2,8 +2,8 @@
 // Utility files to help out and make code more readable
 //
 
-export type ProcessParams = ConstructorParameters<typeof Process>;
-export type ProcessOutput = { stdout: string; stderr: string; status: number };
+export type ProcessParams = ConstructorParameters<typeof Process>
+export type ProcessOutput = { stdout: string; stderr: string; status: number }
 
 /**
  * Run a non-interactive process and get the stdout, stderr & status in one go
@@ -16,42 +16,39 @@ export function execute(
   options: ProcessParams[1]
 ): Promise<ProcessOutput> {
   return new Promise<ProcessOutput>((resolve) => {
-    const process = new Process(path, options);
+    const process = new Process(path, options)
 
     // Copy all stdout into an array of lines
-    const stdout: string[] = [];
-    process.onStdout((line) => stdout.push(line));
+    const stdout: string[] = []
+    process.onStdout((line) => stdout.push(line))
 
     // Copy all stderr into an array of lines
-    const stderr: string[] = [];
-    process.onStderr((line) => stderr.push(line));
+    const stderr: string[] = []
+    process.onStderr((line) => stderr.push(line))
 
     // Resolve the promise once the process has exited,
     // with the stdout and stderr as single strings and the status code number
     process.onDidExit((status) =>
       resolve({
         status,
-        stderr: stderr.join("\n"),
-        stdout: stdout.join("\n"),
+        stderr: stderr.join('\n'),
+        stdout: stdout.join('\n'),
       })
-    );
+    )
 
     // Start the process
-    process.start();
-  });
+    process.start()
+  })
 }
 
 export function copyToExtensionStorage(filename: string) {
-  const source = nova.path.join(nova.extension.path, filename);
-  const destination = nova.path.join(
-    nova.extension.globalStoragePath,
-    filename
-  );
+  const source = nova.path.join(nova.extension.path, filename)
+  const destination = nova.path.join(nova.extension.globalStoragePath, filename)
 
   if (nova.fs.access(destination, nova.fs.constants.F_OK)) {
-    nova.fs.remove(destination);
+    nova.fs.remove(destination)
   }
-  nova.fs.copy(source, destination);
+  nova.fs.copy(source, destination)
 }
 
 /**
@@ -64,21 +61,21 @@ export function copyToExtensionStorage(filename: string) {
  */
 export function createDebug(namespace: string) {
   return (...args: any[]) => {
-    if (!nova.inDevMode()) return;
+    if (!nova.inDevMode()) return
 
     const humanArgs = args.map((arg) =>
-      typeof arg === "object" ? JSON.stringify(arg) : arg
-    );
-    console.info(`${namespace}:`, ...humanArgs);
-  };
+      typeof arg === 'object' ? JSON.stringify(arg) : arg
+    )
+    console.info(`${namespace}:`, ...humanArgs)
+  }
 }
 
 /** Find the full path of a binary */
 export async function findBinaryPath(binary: string): Promise<string | null> {
-  const { stdout, status } = await execute("/usr/bin/env", {
-    args: ["which", binary],
-  });
-  return status === 0 ? stdout.trim() : null;
+  const { stdout, status } = await execute('/usr/bin/env', {
+    args: ['which', binary],
+  })
+  return status === 0 ? stdout.trim() : null
 }
 
 /**
@@ -93,21 +90,21 @@ export function askChoice(
   return new Promise<string | null>((resolve) => {
     workspace.showChoicePalette(choices, { placeholder }, (choice) =>
       resolve(choice)
-    );
-  });
+    )
+  })
 }
 
 /**
  * Output put a potentially unknown error
  */
 export function logError(message: string, error: unknown) {
-  console.error(message);
+  console.error(message)
 
   if (error instanceof Error) {
-    console.error(error.message);
-    console.error(error.stack);
+    console.error(error.message)
+    console.error(error.stack)
   } else {
-    console.error("An non-error was thrown");
-    console.error(error);
+    console.error('An non-error was thrown')
+    console.error(error)
   }
 }
